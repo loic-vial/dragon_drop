9e6fb3bb000cd17e8b8206f00b62682d257b1818
Jade <jade0@hotmail.fr>
2014-06-03 13:52:20 +0200

    root

--- src/ei_application.c
+++ src/ei_application.c
@@ -5,34 +5,41 @@
 #include "ei_widgetclass.h"
 #include "string.h"
 
+ei_widget_t root;
+
 void ei_app_create(ei_size_t* main_window_size, ei_bool_t fullscreen)
 {
     /*initializes the hardware */
     ei_surface_t surface_in;
     void hw_init();
 
-    /* registers all classes of widget and all geometry managers*/
+    /*creates the root widget to accress the root window. */
 
-    /*register of the frame class */
-    ei_widgetclass_t frame;
-    strcpy(frame.name,"frame");
-    frame.allocfunc=&allocfunc_frame;
-    frame.releasefunc=&releasefunc_frame;
-    frame.drawfunc=&drawfunc_frame;
-    frame.setdefaultsfunc=&setdefaultsfunc_frame;
-    frame.geomnotifyfunc=&geomnotifyfunc_frame;
-    frame.next = NULL;
-
+    root.wclass = NULL;
+    root.pick_id=0xFFFFFF;
+    root.pick_color=0xFFFFFF;
+    root.parent=NULL;
+    root.children_head=NULL;
+    root.children_tail;
+    root.next_sibling;
+    root.geom_params=NULL;
+    /*   ei_size_t		requested_size;	///< Size requested by the widget (big enough for its label, for example), or by the programmer. This can be different than its screen size defined by the placer.
+        ei_rect_t		screen_location;///< Position and size of the widget expressed in the root window reference.
+        ei_rect_t*		content_rect;	///< Where to place children, when this widget is used as a container. By defaults, points to the screen_location.
+*/
     /* registers all classes of widget and all geometry managers*/
-    ei_widgetclass_register(&frame); /* TODO mettre toutes les classes*/
+    /* TODO mettre toutes les classes*/
     ei_frame_register_class ();
 
-
-surface_in = hw_create_window(main_window_size, (const ei_bool_t) fullscreen);
+    /*creates the root window (either in a system window, or the entire
+ *				screen),*/
+    surface_in = hw_create_window(main_window_size, (const ei_bool_t) fullscreen);
 }
 
 void ei_app_run()
 {
+    // pour chaque widget
+    // on recupere la classe, donc la fonction de draw
 
     getchar();
 }
@@ -51,6 +58,7 @@ void ei_app_quit_request()
 ei_widget_t* ei_app_root_widget()
 {
     NULL;
+    // on retourne le widget "root" déclaré en global ici tout en haut
 }
 
 
--- src/ei_widgetclass.c
+++ src/ei_widgetclass.c
@@ -1,12 +1,18 @@
 #include "ei_widgetclass.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "ei_frame.h"
 
+ei_widgetclass_t first_class;
 
-
-void			ei_widgetclass_register		(ei_widgetclass_t* widgetclass)
+void ei_widgetclass_register		(ei_widgetclass_t* widgetclass)
 {
-    NULL;
+    ei_widgetclass_t tmp=first_class;
+   /* while(tmp.next /= NULL)
+    {
+       tmp=first_class.next;
+    }
+    tmp.next=widgetclass;*/
 }
 
 ei_widgetclass_t*	ei_widgetclass_from_name	(ei_widgetclass_name_t name)
@@ -16,7 +22,15 @@ ei_widgetclass_t*	ei_widgetclass_from_name	(ei_widgetclass_name_t name)
 
 void			ei_frame_register_class 	()
 {
-    NULL;
+    ei_widgetclass_t frame;
+    strcpy(frame.name,"frame");
+    frame.allocfunc=&allocfunc_frame;
+    frame.releasefunc=&releasefunc_frame;
+    frame.drawfunc=&drawfunc_frame;
+    frame.setdefaultsfunc=&setdefaultsfunc_frame;
+    frame.geomnotifyfunc=&geomnotifyfunc_frame;
+    frame.next = NULL;
+    ei_widgetclass_register(&frame);
 }
 
 void			ei_button_register_class 	()
