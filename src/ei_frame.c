81e3bf6e9f9299a7018846f10f5be4fb97215d8b
ROMAN Laura-Dimitrita <you@example.com>
2014-06-07 15:07:55 +0200

    rounded rounded <3

--- src/ei_frame.c
+++ src/ei_frame.c
@@ -24,14 +24,14 @@ ei_linked_point_t arc(ei_point_t centre, float rayon, float angle_debut, float a
     ei_point_t point;
 
     for(float angle=angle_debut; angle<angle_fin; angle+=1){
-        point.x = centre.x + rayon*cos(angle*PI/180);
-        point.y = centre.y + rayon*sin(angle*PI/180);
+        point.x = centre.x + rayon*cos(angle*PI/180.0);
+        point.y = centre.y - rayon*sin(angle*PI/180.0);
         current_point->point = point;
         current_point->next = malloc(sizeof(ei_linked_point_t));
         current_point = current_point->next;
     }
-    point.x = centre.x + rayon*cos(angle_fin*PI/180);
-    point.y = centre.y + rayon*sin(angle_fin*PI/180);
+    point.x = centre.x + rayon*cos(angle_fin*PI/180.0);
+    point.y = centre.y - rayon*sin(angle_fin*PI/180.0);
     current_point->point = point;
     current_point->next = NULL;
     return(liste_points);
@@ -41,26 +41,33 @@ ei_linked_point_t rounded_frame(ei_rect_t rectangle, float rayon){
     ei_point_t sommet;
     ei_point_t centre;
     ei_linked_point_t liste_points;
-    ei_linked_point_t* current_point = &liste_points;
+    ei_linked_point_t* current_point;
     //haut gauche
     sommet = rectangle.top_left;
     centre.x = sommet.x + rayon;
-    centre.y = sommet.y - rayon;
+    centre.y = sommet.y + rayon;
     liste_points = arc(centre, rayon, 90, 180);
     current_point = &liste_points;
     while (current_point->next != NULL) {
         current_point = current_point->next;
     }
-    //haut droite
-    sommet.x += rectangle.size.width;
+    //bas gauche
+    sommet.y += rectangle.size.height;
+    centre.y = sommet.y - rayon;
+    current_point->next = malloc(sizeof(ei_linked_point_t));
+    *current_point->next = arc(centre, rayon, -180, -90);
+    while (current_point->next != NULL) {
+        current_point = current_point->next;
+    }
+    //bas droite
+    sommet.x += +rectangle.size.width;
     centre.x = sommet.x - rayon;
     current_point->next = malloc(sizeof(ei_linked_point_t));
     *current_point->next = arc(centre, rayon, -90, 0);
     while (current_point->next != NULL) {
         current_point = current_point->next;
     }
-
-    //bas droite
+    //haut droite
     sommet.y += -rectangle.size.height;
     centre.y = sommet.y + rayon;
     current_point->next = malloc(sizeof(ei_linked_point_t));
@@ -68,15 +75,6 @@ ei_linked_point_t rounded_frame(ei_rect_t rectangle, float rayon){
     while (current_point->next != NULL) {
         current_point = current_point->next;
     }
-    //bas gauche
-    sommet.x += -rectangle.size.width;
-    centre.x = sommet.x + rayon;
-    current_point->next = malloc(sizeof(ei_linked_point_t));
-    *current_point->next = arc(centre, rayon, 90, 180);
-    while (current_point->next != NULL) {
-        current_point = current_point->next;
-    }
-
     current_point = NULL;
     //return
     return(liste_points);
@@ -86,24 +84,11 @@ void drawfunc_frame(ei_widget_t* widget, ei_surface_t surface,
                     ei_surface_t pick_surface, ei_rect_t*  clipper)
 {
     ei_frame_t* frame = (ei_frame_t*)widget;
-    ei_linked_point_t first_point;
-    ei_linked_point_t second_point;
-    ei_linked_point_t third_point;
-    ei_linked_point_t fourth_point;
-    first_point.point = widget->screen_location.top_left;
-    first_point.next = &second_point;
-    second_point.point = widget->screen_location.top_left;
-    second_point.point.x += widget->screen_location.size.width;
-    second_point.next = &third_point;
-    third_point.point = second_point.point;
-    third_point.point.y += widget->screen_location.size.height;
-    third_point.next = &fourth_point;
-    fourth_point.point = first_point.point;
-    fourth_point.point.y += widget->screen_location.size.height;
-    fourth_point.next = NULL;
 
+    ei_rect_t rect = widget->screen_location;
+    ei_linked_point_t points = rounded_frame(rect, 15);
 
-    ei_draw_polygon(surface, &first_point, frame->color, clipper);
+    ei_draw_polygon(surface, &points, frame->color, clipper);
 
     if (frame->text != NULL)
     {
@@ -144,7 +129,7 @@ void drawfunc_frame(ei_widget_t* widget, ei_surface_t surface,
 
         hw_surface_unlock(*frame->img);
     }
-    ei_draw_polygon(pick_surface, &first_point, *widget->pick_color, clipper);
+    ei_draw_polygon(pick_surface, &points, *widget->pick_color, clipper);
 
     if (frame->text != NULL)
     {
--- tests/frame.c
+++ tests/frame.c
@@ -13,7 +13,7 @@
  *
  *	Main function of the application.
  */
-int _ei_main(int argc, char** argv)
+int ____ei_main(int argc, char** argv)
 {
 	ei_size_t	screen_size		= {600, 600};
     ei_color_t	root_bgcol		= {0x52, 0x7f, 0xb4, 0xff};
