84359b6afee088e44323c7666ff3aafa081dde78
ROMAN Laura-Dimitrita <you@example.com>
2014-06-11 15:28:33 +0200

    presque

--- tests/frame.c
+++ tests/frame.c
@@ -6,6 +6,7 @@
 #include "hw_interface.h"
 #include "ei_widget.h"
 #include "ei_geometrymanager.h"
+#include "../src/ei_frame.h"
 
 
 /*
@@ -13,7 +14,7 @@
  *
  *	Main function of the application.
  */
-int _______________________ei_main(int argc, char** argv)
+int ei_main(int argc, char** argv)
 {
 	ei_size_t	screen_size		= {600, 600};
     ei_color_t	root_bgcol		= {0x52, 0x7f, 0xb4, 0xff};
@@ -32,6 +33,14 @@ int _______________________ei_main(int argc, char** argv)
 
 	/* Create, configure and place the frame on screen. */
     frame = ei_widget_create("frame", ei_app_root_widget());
+
+
+    ei_frame_t* fframe = (ei_frame_t*) frame;
+
+    fframe->rounded_up = EI_TRUE;
+    fframe->rounded_down = EI_TRUE;
+    fframe->corner_radius = 5;
+
     ei_frame_configure(frame, &frame_size, &frame_color,
                 &frame_border_width, &frame_relief, NULL, NULL, NULL, NULL,
                 NULL, NULL, NULL);
