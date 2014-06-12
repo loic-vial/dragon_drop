f3f75b90df1d927d5db75fdde9b59d2a87b0b107
Jade <jade0@hotmail.fr>
2014-06-12 17:52:38 +0200

    tag

--- src/ei_tag.c
+++ src/ei_tag.c
@@ -1,6 +1,8 @@
 #include "ei_tag.h"
 #include <stdlib.h>
+#include <string.h>
 #include "ei_widgetclass.h"
+#include "ei_frame.h"
 
 ei_linked_tag_t* ei_initial_tag_t(ei_widget_t* widget)
 {
@@ -11,4 +13,30 @@ ei_linked_tag_t* ei_initial_tag_t(ei_widget_t* widget)
     all->next=name;
     name->next=NULL;
     return all;
+}
+
+void ei_add_tag(ei_widget_t* widget,ei_tag_t* tag)
+{
+    ei_linked_tag_t* new_tag=(ei_linked_tag_t*)malloc(sizeof(ei_linked_tag_t));
+    new_tag->next=NULL;
+    new_tag->tag=*tag;
+    ei_frame_t* frame=(ei_frame_t*)widget;
+    ei_linked_tag_t* tag_liste = frame->tag;
+    while (tag_liste->next !=NULL)
+    {
+        tag_liste=tag_liste->next;
+    }
+    tag_liste->next=new_tag;
+}
+
+void ei_destroy_tag(ei_widget_t* widget,ei_tag_t* tag)
+{
+    ei_frame_t* frame=(ei_frame_t*)widget;
+    ei_linked_tag_t* tag_liste = frame->tag;
+    while (strcmp((char*)tag_liste->next->tag,(char*)tag) !=0)
+    {
+        tag_liste=tag_liste->next;
+    }
+    tag_liste->next=tag_liste->next->next; //TODO desalouer le tag detruit
+
 }
--- src/ei_tag.h
+++ src/ei_tag.h
@@ -5,5 +5,9 @@
 
  ei_linked_tag_t* ei_initial_tag_t(ei_widget_t* widget);
 
+ void ei_add_tag(ei_widget_t* widget,ei_tag_t* tag);
+
+ void ei_destroy_tag(ei_widget_t* widget,ei_tag_t* tag);
+
 
 #endif
