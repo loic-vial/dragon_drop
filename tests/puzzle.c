82996e432503a1a1c1a600c40915790ccef3486b
Jade <jade0@hotmail.fr>
2014-06-13 11:44:27 +0200

    tag ok !

--- tests/puzzle.c
+++ tests/puzzle.c
@@ -191,7 +191,7 @@ ei_bool_t handle_keydown(ei_widget_t* widget, ei_event_t* event, void* user_para
 
 
 
-int ei_main(int argc, char* argv[])
+int kkei_main(int argc, char* argv[])
 {
     ei_size_t	root_window_size		= { 800, 800 };
 	ei_color_t	root_bgcol			= { 0x52, 0x7f, 0xb4, 0xff };
--- tests/test_tag.c
+++ tests/test_tag.c
@@ -7,8 +7,10 @@
 #include "ei_widget.h"
 #include "ei_geometrymanager.h"
 #include "../src/ei_frame.h"
-#include "../src/ei_tag.h"
+#include "ei_tag.h"
+#include "ei_utils_2.h"
 
+ei_widget_t* frame_tooltip;
 
 ei_bool_t quit(ei_widget_t* widget, ei_event_t* event, void* user_param)
 {
@@ -19,6 +21,30 @@ ei_bool_t quit(ei_widget_t* widget, ei_event_t* event, void* user_param)
 
     return EI_FALSE;
 }
+
+
+
+
+ei_bool_t display(ei_widget_t* widget, ei_event_t* event, void* user_param)
+{
+      ei_widget_t* widget_tooltip=(ei_widget_t*)frame_tooltip;
+    if(widget_has_this_tag(widget,"tooltip"))
+    {
+    ei_tail(widget_tooltip);
+    ei_point_t location = widget->screen_location.top_left;
+    location.y+=200;
+    ei_place(frame_tooltip, NULL ,&location.x, &location.y, NULL, NULL, NULL, NULL, NULL, NULL );
+    }
+    else
+    {
+        ei_geometrymanager_unmap(widget_tooltip);
+    }
+    return EI_FALSE;
+}
+
+
+
+
 
 void display_list_tag()
 {
@@ -41,18 +67,17 @@ void display_tag(ei_widget_t* widget)
     }
 }
 
-int ooei_main(int argc, char** argv)
+int ei_main(int argc, char** argv)
 {
     ei_size_t	screen_size		= {600, 600};
     ei_color_t	root_bgcol		= {0x52, 0x7f, 0xb4, 0xff};
 
     ei_widget_t*	frame;
-    ei_size_t	frame_size		= {300,200};
-    int		frame_x			= 150;
-    int		frame_y			= 200;
+    ei_size_t	frame_size		= {200,200};
+
     ei_color_t	frame_color		= {0x40, 0xA4, 0x97, 0xff};
-    ei_relief_t	frame_relief		= ei_relief_raised;
-    int		frame_border_width	= 8;
+    ei_relief_t	frame_relief		= ei_relief_none;
+    int		frame_border_width	= 1;
 
     /* Create the application and change the color of the background. */
     ei_app_create(&screen_size, EI_FALSE);
@@ -62,37 +87,71 @@ int ooei_main(int argc, char** argv)
     frame = ei_widget_create("frame", ei_app_root_widget());
 
 
-    ei_frame_t* fframe = (ei_frame_t*) frame;
 
-    fframe->rounded_up = EI_TRUE;
-    fframe->rounded_down = EI_TRUE;
-    fframe->corner_radius = 15;
-
+    char* text = "Une tooltip existe ici";
     ei_frame_configure(frame, &frame_size, &frame_color,
-                       &frame_border_width, &frame_relief, NULL, NULL, NULL, NULL,
+                       &frame_border_width, &frame_relief, &text, NULL, NULL, NULL,
                        NULL, NULL, NULL);
-    ei_place(frame, NULL, &frame_x, &frame_y, NULL, NULL, NULL, NULL, NULL, NULL );
+    ei_anchor_t anc=ei_anc_west;
+    ei_place(frame,  &anc,NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL );
+    ei_tag_t tag = (ei_tag_t)"tooltip";
+    ei_add_tag_widget(frame,tag);
 
+    /*Create a second frame*/
+    ei_widget_t* frame2 = ei_widget_create("frame", ei_app_root_widget());
+    text="Il n'en existe pas ici";
+    ei_frame_configure(frame2, &frame_size, &frame_color,
+                       &frame_border_width, &frame_relief, &text, NULL, NULL, NULL,
+                       NULL, NULL, NULL);
+    anc=ei_anc_center;
+    ei_place(frame2,  &anc,NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL );
+
+
+
+    /*Create a third frame*/
+    ei_widget_t* frame3 = ei_widget_create("frame", ei_app_root_widget());
+    text="Il en existe une ici";
+    ei_frame_configure(frame3, &frame_size, &frame_color,
+                       &frame_border_width, &frame_relief, &text, NULL, NULL, NULL,
+                       NULL, NULL, NULL);
+    anc=ei_anc_east;
+    ei_place(frame3,  &anc,NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL );
+    ei_add_tag_widget(frame3,tag);
+
+
+    /*Create a frame for display*/
+    frame_tooltip = ei_widget_create("frame", ei_app_root_widget());
+    text="Je suis une tooltip";
+    ei_size_t	frame_tooltip_size		= {100,50};
+    ei_color_t color ={0xff,0xff,0xff,50};
+    ei_frame_configure(frame_tooltip, &frame_tooltip_size, &color,
+                       &frame_border_width, &frame_relief, &text, NULL, NULL, NULL,
+                       NULL, NULL, NULL);
+
+    /*test d'ajout et de suppression*/
     printf("On affiche la liste initiale de tag \n");
     display_list_tag();
     printf("on ajoute le tag bullet au widget fframe \n");
-    ei_tag_t tag = (ei_tag_t)"bullet";
+
+    tag = (ei_tag_t)"bullet";
     ei_add_tag_widget(frame,tag);
     display_list_tag();
     printf("On affiche les tag de frame \n");
     display_tag(frame);
-    printf("On va detruire all dans frame \n");
-    tag=(ei_tag_t)"all";
+    printf("On va detruire frame dans frame \n");
+    tag=(ei_tag_t)"frame";
 
     ei_destroy_tag_widget(frame,tag);
     display_tag(frame);
     printf("on va detruire le tag frame dans tout les widget \n");
     tag=(ei_tag_t)"frame";
-ei_destroy_tag(tag);
-display_list_tag();
+    ei_destroy_tag(tag);
+    display_list_tag();
 
 
     ei_bind(ei_ev_keydown, 		NULL, "all", quit, NULL);
+
+    ei_bind(ei_ev_mouse_move,NULL, "all", display, NULL);
 
     ei_app_run();
 
