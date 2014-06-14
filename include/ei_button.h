588235cddf1145404e0343e2313d70c024287e4c
Jade <jade0@hotmail.fr>
2014-06-14 09:42:49 +0200

    commentaire

--- include/ei_button.h
+++ include/ei_button.h
@@ -3,22 +3,54 @@
 
 #include "ei_frame.h"
 
+
+/**
+ * \brief	ei_button_t     Button is a widget who has lot of common fields with frame and two for itself.
+ */
 typedef struct ei_button_t
 {
-    ei_frame_t frame;
-    ei_callback_t callback;
-    void* user_param;
+    ei_frame_t frame; ///<It's a pointer to a widget and a frame. Many fields of frame are shared by others widgets.
+    ei_callback_t callback; ///< This function will be called then the button is clicked
+    void* user_param; ///<Parameter send by the user
 }
 ei_button_t;
 
+/**
+ * @brief	ei_button_allocfunc     A function that is called to allocate a block of the size of a \ref ei_button_t in the memory.
+ *                                  It initialize it to zero.
+ *
+ */
 void* ei_button_allocfunc();
 
+/**
+ * @brief ei_button_releasefunc A function that is called to release the memory used by the widget button.
+ * @param widget    This widget is a button that must be released.
+ */
 void ei_button_releasefunc(ei_widget_t* widget);
 
+/**
+ * @brief ei_button_setdefaultsfunc     A function that set parameters to defaults values.
+ * @param widget                        The button widget to set to default. It used the \ref ei_frame_setdefaultsfunc function.
+ *                                      Callback and user_param are set to NULL.
+ */
 void ei_button_setdefaultsfunc(ei_widget_t* widget);
 
+/**
+ * @brief ei_button_click_down      A callback which is called when a button is clicked.
+ * @param widget                    The widget button which is clicked
+ * @param event                     The event \ref ei_ev_mouse_buttondown
+ * @param user_param                Parameters send by the user
+ * @return
+ */
 ei_bool_t ei_button_click_down(ei_widget_t* widget, ei_event_t* event, void* user_param);
 
+/**
+ * @brief ei_button_click_up        A callback which is called then a clicked button is unclicked
+ * @param widget                    The widget button which is released
+ * @param event                     The event \ref ei_ev_mouse_buttonup
+ * @param user_param                Parameters send by the user
+ * @return
+ */
 ei_bool_t ei_button_click_up(ei_widget_t* widget, ei_event_t* event, void* user_param);
 
 #endif
--- include/ei_frame.h
+++ include/ei_frame.h
@@ -4,35 +4,66 @@
 #include "ei_widget.h"
 #include "ei_tag.h"
 
+/**
+* \brief ei_frame_t     A struct for a frame
+*
+*/
 typedef struct ei_frame_t
 {
-    ei_widget_t widget;
-    ei_color_t color;
-    int border_width;
-    ei_relief_t relief;
-    char* text;
-    ei_font_t text_font ;
-    ei_color_t text_color;
-    ei_anchor_t text_anchor;
-    ei_surface_t img;
-    ei_rect_t* img_rect;
-    ei_anchor_t img_anchor;
-    int corner_radius;
-    ei_bool_t rounded_up;
-    ei_bool_t rounded_down;
-    ei_linked_tag_t* tag;
+    ei_widget_t widget; ///< A widget to memorize the place in the hierarchie of widget
+    ei_color_t color; ///< Color of the frame
+    int border_width; ///<A border width around the frame
+    ei_relief_t relief; /// Relief of the frame, the size of the border width
+    char* text; ///< Text inside the frame
+    ei_font_t text_font ; ///<Font of the text
+    ei_color_t text_color; ///< Color of the text
+    ei_anchor_t text_anchor;///Anchor of the text
+    ei_surface_t img;///< Image of the frame. The frame can't has a text and a image
+    ei_rect_t* img_rect; ///< Rectangle to say wich part of the image will be displayed
+    ei_anchor_t img_anchor; ///< anchor of the image
+    int corner_radius; ///<Radius of the corner of thr frame
+    ei_bool_t rounded_up; ///< True if the two corner in the top of the frame are rounded
+    ei_bool_t rounded_down;///< True if the two corner in the bottom of the frame are rounded
+    ei_linked_tag_t* tag; ///< List of tag
 }
 ei_frame_t;
 
+/**
+ * @brief ei_frame_allocfunc    A function that allocate a block in the memory the size of a \ref ei_frame_t, initialized to zero
+ * @return
+ */
 void* ei_frame_allocfunc();
 
+/**
+ * @brief ei_frame_releasefunc  A function that released all parameters of a frame
+ * @param widget
+ */
 void ei_frame_releasefunc(ei_widget_t* widget);
 
+/**
+ * @brief ei_frame_drawfunc     A function that draw a frame
+ * @param widget                The frame to draw
+ * @param surface               The root surface where to draw the widget
+ * @param pick_surface          The offscreen surface where to draw the widget with it pick_color
+ * @param clipper               A surface that delimited the surface where to draw the widget
+ */
 void ei_frame_drawfunc(ei_widget_t* widget, ei_surface_t surface,
                       ei_surface_t pick_surface, ei_rect_t* clipper);
 
+/**
+ * @brief ei_frame_setdefaultsfunc      A function that set to default all parameters of a frame
+ * @param widget                        The frame widget to set to defaults values
+ */
 void ei_frame_setdefaultsfunc(ei_widget_t* widget);
 
+/**
+ * @brief arc           A function that create a list a point in a circle, and only a part of this circle is returned
+ * @param centre        Center of the circle
+ * @param radius        Radius of the circle
+ * @param angle_debut   Angle where is the first point
+ * @param angle_fin     Angle where is the last point
+ * @return              A list of point wich represent only a piece of circle
+ */
 ei_linked_point_t* arc(ei_point_t centre, float radius, float angle_debut, float angle_fin);
 
 #endif
